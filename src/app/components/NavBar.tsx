0842eeefebf0ee358ca3616520d981169e8d618f
Abhishek Ritu <abhishekritu810219@gmail.com>
2025-05-11 19:53:21 +0530

    check

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -150,7 +150,7 @@ const NavBar = () => {
             <FaUserPlus /> Register Now
           </a>
           <a
-            href="tel:+918951258207"
+             href="https://wa.me/+918951258207?text=Hi,%20I%20would%20like%20to%20know%20more%20about%20your%20programs!%0A"
             className="flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-orange-500/20 border border-orange-400 text-orange-300 hover:text-white text-sm"
             onClick={() => setMenuOpen(false)}
           >
