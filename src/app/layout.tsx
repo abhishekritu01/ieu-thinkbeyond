ad3736b45e9689fb99f518a72a2322b0344ab853
hbphaneesh <66403147+hbphaneesh@users.noreply.github.com>
2025-05-27 16:22:54 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17086159771"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17086159771');
+</script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
